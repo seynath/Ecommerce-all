b83ad48eb476b14aa3b6241508a12dc6dc347b13
Seynath Thenura <117857144+seynath@users.noreply.github.com>
2024-07-11 22:56:45 +0530

    Update baseUrl.jsx

--- admin/src/utils/baseUrl.jsx
+++ admin/src/utils/baseUrl.jsx
@@ -1,10 +1,10 @@
 // Import Vite's environment variables
-export const base_url = import.meta.env.VITE_BASE_URL;
+// export const base_url = import.meta.env.VITE_BASE_URL;
 
 // import dotenv from 'dotenv';
 // dotenv.config();
 
 
 
-// // export const base_url = "http://localhost:5001/api/";
+export const base_url = "http://localhost:5001/api/";
 // export const base_url = process.env.BASE_URL;
