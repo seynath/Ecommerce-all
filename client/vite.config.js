707ea046cd0aa252d64da105c1c0cc9cfba7e40c
Seynath Thenura <117857144+seynath@users.noreply.github.com>
2024-07-19 10:33:51 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  // Load env file based on the current mode (`.env`, `.env.production`, `.env.development`, etc.)
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [react()],
+    define: {
+      'process.env.VITE_BASE_URL': JSON.stringify(env.VITE_BASE_URL),
+    },
+  };
+});
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
