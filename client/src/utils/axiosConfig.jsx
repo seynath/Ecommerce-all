0e2c8ce42f96a571d9e204f500291493e9aaf489
seynath <thenura-im20102@stu.kln.ac.lk>
2024-07-19 13:20:32 +0530

    Update axiosConfig.jsx

--- client/src/utils/axiosConfig.jsx
+++ client/src/utils/axiosConfig.jsx
@@ -1,6 +1,6 @@
 // export const base_url = "http://34.67.30.220:5001/api/";
-// export const base_url = import.meta.env.VITE_BASE_URL;
-export const base_url = process.env.VITE_BASE_URL;
+export const base_url = import.meta.env.VITE_BASE_URL;
+// export const base_url = process.env.VITE_BASE_URL;
 
 
 const getTokenFromLocalStorage = localStorage.getItem("user")
